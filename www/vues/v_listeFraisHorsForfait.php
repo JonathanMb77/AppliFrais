a69e8a52adb2a982faffc82326c98c4337dc1bcc
Jonathan MBELOLO <jonathan.mbelolo@lycee-basch.fr>
2024-04-11 17:17:43 +0200

    DE04

--- www/vues/v_listeFraisHorsForfait.php
+++ www/vues/v_listeFraisHorsForfait.php
@@ -30,7 +30,7 @@
 	?>	  
                                           
     </table>
-      <form action="index.php?uc=gererFrais&amp;action=validerCreationFrais" method="post">
+      <form action="index.php?uc=gererFrais&amp;action=validerCreationFrais" method="post" enctype="multipart/form-data">
       <div class="corpsForm">
          
           <fieldset>
@@ -48,12 +48,10 @@
               <label for="txtMontantHF">Montant : </label>
               <input type="text" id="txtMontantHF" name="montant" size="10" maxlength="10" value="" />
             </p>
-            <form action="upload.php" method="post" enctype="multipart/form-data">
-              Ajouter votre/vos justificatif(s):
-              <input type="file" name="the_file" id="fileToUpload" accept=".pdf">
-              <!--<input type="submit" value="Upload Image" name="submit">-->
-              <input id="ajouter" type="submit" name="submit" value="Ajouter" size="20" />
-            </form>
+            Ajouter votre/vos justificatif(s):
+            <input type="file" name="the_file" id="fileToUpload" accept=".pdf">
+            <!--<input type="submit" value="Upload Image" name="submit">-->
+            <input id="ajouter" type="submit" name="submit" value="Ajouter" size="20" />
           </fieldset>
       </div>
       <div class="piedForm">
